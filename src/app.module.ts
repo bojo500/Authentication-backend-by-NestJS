6b37063a6338e87ef39b70cf91addcafcf9a6e9b
bojo500 <flavasava1212@gmail.com>
2024-08-13 19:10:39 +0300

    - set the database config - set swagger ui doc config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { User } from "./users/entities";
+import { features } from "./index";
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DATABASE_HOST || 'localhost',
+      port: parseInt(process.env.DATABASE_PORT, 10) || 8886,
+      username: process.env.DATABASE_USER || 'admin',
+      password: process.env.DATABASE_PASSWORD || '^1Wr04yB!NF8',
+      database: process.env.DATABASE_NAME || 'authDB',
+      entities: [User],
+      synchronize: true,
+      logging: true,
+    }),
+    ...features,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,38 @@
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+dotenv.config();
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.use(cors({
+    origin: ['http://localhost:3000', 'http://mo.com:3000'],
+    optionsSuccessStatus: 200,
+    methods: 'GET,POST',
+    allowedHeaders: 'Content-Type,Authorization',
+  }));
+  const port = process.env.PORT || 9001;
+
+  const config = new DocumentBuilder()
+    .setTitle('Authentication System Open API 🚀')
+    .setDescription('Authentication System API description 📋')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
+  app.enableCors({
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
+  await app.listen(port);
+  console.log(`Application is running 🚀 on: ${await app.getUrl()} Author : Mohamed Khaled`);
 }
+
 bootstrap();
