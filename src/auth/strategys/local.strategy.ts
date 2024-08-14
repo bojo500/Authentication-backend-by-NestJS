4a12fbb9079ac7070ec7ddb4a5c9eb1d3da462e0
bojo500 <flavasava1212@gmail.com>
2024-08-14 17:41:54 +0300

    feat(sendig emails): set the sendgrid integration config
    add the sendgrid config to send an email to the user email to verify it

--- src/auth/strategys/local.strategy.ts
+++ src/auth/strategys/local.strategy.ts
@@ -10,9 +10,7 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(email: string, password: string): Promise<any> {
-    console.log({email,password})
     const user = await this.authService.validateUser(email, password);
-    console.log({userlocal: user})
     if (!user) {
       throw new UnauthorizedException();
     }
